ee6896d0de647ddf2d52f1db2d36474da3f83e6f
Luis del Cueto <luisdelcueto@live.com>
2017-05-10 13:11:03 -0400

    Done.

--- twitter.rb
+++ twitter.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'yaml'
+require "pry"
 
 class TwitterApi
   attr_reader :client
@@ -14,32 +15,77 @@ class TwitterApi
     end
   end
 
-
   def most_recent_friend
     #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    client.friends.first
   end
 
   def find_user_for(username)
     #find the twitter gem method that returns the correct user, given a username
+    client.user(username)
   end
 
   def find_followers_for(username)
     #find the twitter gem method that returns the follows of a given user
+    array = client.followers(username).take(10)
   end
 
   def homepage_timeline
     #find the twitter gem method that retreives the tweets from a user's timeline.
+    array = client.home_timeline
   end
-  
 end
 
-#Bonus: 
+#Bonus:
 
 # uncomment out the following and read the bonus instructions.
 # remember to comment out the WebMock line of your spec_helper, as the instructions dictate.
-
+# class TwitterApi
+#   attr_reader :client
+#
+#   def initialize
+#     keys = YAML.load_file('application.yml')
+#     @client = Twitter::REST::Client.new do |config|
+#       config.consumer_key        = keys['CONSUMER_KEY']
+#       config.consumer_secret     = keys['CONSUMER_SECRET']
+#       config.access_token        = keys['ACCESS_TOKEN']
+#       config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+#     end
+#   end
+#
+#   def most_recent_friend
+#     #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+#     client.friends.first.name
+#   end
+#
+#   def find_user_for(username)
+#     #find the twitter gem method that returns the correct user, given a username
+#     client.user(username).name
+#   end
+#
+#   def find_followers_for(username)
+#     #find the twitter gem method that returns the follows of a given user
+#     array = client.followers(username).take(10)
+#     names = []
+#     array.each do |x|
+#       names << x.name
+#     end
+#     names
+#   end
+#
+#   def homepage_timeline
+#     #find the twitter gem method that retreives the tweets from a user's timeline.
+#     array = client.home_timeline
+#     tweets = []
+#     array.each do |x|
+#       tweets << x.text
+#     end
+#     tweets
+#   end
+# end
+#
 # tweet_client = TwitterApi.new
 # puts tweet_client.most_recent_friend
-# puts tweet_client.find_user_for("USERNAME HERE")
-# puts tweet_client.find_followers_for("USERNAME HERE")
+# puts tweet_client.find_user_for("Luisdelcue")
+# puts tweet_client.find_followers_for("Luisdelcue")
 # puts tweet_client.homepage_timeline
